2c237a48794f1e3776aba7420308621b161f0f96
art1722 <Artistitsart172@gmail.com>
2024-04-19 15:17:43 +0700

    add API endpoint for frontend

---
+++ frontend/public/scripts/api.js
@@ -0,0 +1,37 @@
+import { BACKEND_URL } from "./config.js";
+
+export async function getLeaderboard() {
+    const leaderboard = await fetch(`${BACKEND_URL}/leaderboard`).then((r) => r.json());
+
+    return leaderboard;
+}
+
+export async function getMap() {
+    const canvas = await fetch(`${BACKEND_URL}/tiles/map-info`).then((r) => r.json());
+ 
+    return canvas;
+}
+
+export async function getTile(x, y) {
+    const tile = await fetch(`${BACKEND_URL}/tiles/tile-info`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({x, y}),
+    }).then((r) => r.json());
+ 
+    return tile;
+}
+
+export async function createTiles(item) {
+    const tile = await fetch(`${BACKEND_URL}/tiles`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item),
+    });
+
+    return tile
+}
