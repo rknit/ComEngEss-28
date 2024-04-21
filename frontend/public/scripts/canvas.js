c16b73b4327b6d7267a308b18e193a8929e6d405
Rachata <rachatakanit@gmail.com>
2024-04-22 04:42:43 +0700

    handle mobile events

--- frontend/public/scripts/canvas.js
+++ frontend/public/scripts/canvas.js
@@ -8,37 +8,45 @@ const CANVAS_SIZE = 256;
 const MIN_CELL_SIZE = 10;
 const MAX_CELL_SIZE = 100;
 const ZOOM_SPEED = 3;
-const enableLineFade = false;
-const enableHoverBox = true;
+const ENABLE_LINE_FADE = false;
+const ENABLE_HOVER_BOX = true;
 
 const colors = new Array(CANVAS_SIZE)
 	.fill(0)
 	.map(() => new Array(CANVAS_SIZE).fill("#FFFFFF"));
+const queuedColors = new Map();
 
+let dragging = false;
 let cellSize = MIN_CELL_SIZE;
 let posX = 0;
 let posY = 0;
-let dragging = false;
 let hoverX = -1;
 let hoverY = -1;
 
+// mobiles
+let zooming = false;
+let lastTouchPosX = 0;
+let lastTouchPosY = 0;
+let lastTouchesDist = 0;
+
 function draw() {
-	context.canvas.width = window.innerWidth;
-	context.canvas.height = window.innerHeight;
 	context.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
 	let xLimit = Math.min(CANVAS_SIZE * cellSize + posX, window.innerWidth);
 	let yLimit = Math.min(CANVAS_SIZE * cellSize + posY, window.innerHeight);
 
+	let startX = posX % cellSize;
+	let startY = posY % cellSize;
+
 	context.beginPath();
-	for (let y = posY; y < yLimit; y += cellSize) {
-		for (let x = posX; x < xLimit; x += cellSize) {
+	for (let y = startY; y < yLimit; y += cellSize) {
+		for (let x = startX; x < xLimit; x += cellSize) {
 			let gridX = Math.floor((x - posX) / cellSize);
 			let gridY = Math.floor((y - posY) / cellSize);
-			if (gridX < 0 || gridY < 0) continue;
-			if (gridX >= CANVAS_SIZE || gridY >= CANVAS_SIZE) continue;
-			if (colors[gridX][gridY] == "#FFFFFF") continue;
-			context.fillStyle = colors[gridX][gridY];
+			if (queuedColors.has(`${gridX},${gridY}`))
+				context.fillStyle = queuedColors.get(`${gridX},${gridY}`);
+			else if (colors[gridX][gridY] == "#FFFFFF") continue;
+			else context.fillStyle = colors[gridX][gridY];
 			context.fillRect(x, y, cellSize, cellSize);
 
 			// context.fillStyle = "black";
@@ -52,22 +60,20 @@ function draw() {
 	}
 
 	context.beginPath();
-	for (let x = posX; x <= xLimit; x += cellSize) {
-		if (x < 0 || x > window.innerWidth) continue;
-		context.moveTo(x, posY);
-		context.lineTo(x, yLimit);
+	for (let x = startX; x <= xLimit; x += cellSize) {
+		context.moveTo(x - 0.5, startY);
+		context.lineTo(x - 0.5, yLimit);
 	}
-	for (let y = posY; y <= yLimit; y += cellSize) {
-		if (y < 0 || y > window.innerHeight) continue;
-		context.moveTo(posX, y);
-		context.lineTo(xLimit, y);
+	for (let y = startY; y <= yLimit; y += cellSize) {
+		context.moveTo(startX, y - 0.5);
+		context.lineTo(xLimit, y - 0.5);
 	}
 	context.strokeStyle = "#d9d9d9";
-	if (enableLineFade)
+	if (ENABLE_LINE_FADE)
 		context.lineWidth = Math.sqrt((cellSize - 9.9) / MAX_CELL_SIZE);
 	context.stroke();
 
-	if (!enableHoverBox || hoverX < 0 || hoverY < 0) return;
+	if (!ENABLE_HOVER_BOX || hoverX < 0 || hoverY < 0) return;
 	if (hoverX >= CANVAS_SIZE || hoverY >= CANVAS_SIZE) return;
 	let pixelX = hoverX * cellSize;
 	let pixelY = hoverY * cellSize;
@@ -87,25 +93,26 @@ async function paintFromDatabase() {
 	draw();
 }
 
-function paint(x, y) {
+async function paint(x, y) {
 	if (isOnCooldown()) return;
 
 	let gridX = Math.floor((x - posX) / cellSize);
 	let gridY = Math.floor((y - posY) / cellSize);
 	if (gridX < 0 || gridY < 0 || gridX >= CANVAS_SIZE || gridY >= CANVAS_SIZE)
 		return;
-	colors[gridX][gridY] =
-		document.getElementById("color-button").style.backgroundColor;
-	draw();
+	if (queuedColors.has(`${gridX},${gridY}`)) return;
 
+	let color = document.getElementById("color-button").style.backgroundColor;
 	let team = localStorage.getItem("team");
-	createTile({ x: gridX, y: gridY, color: colors[gridX][gridY], team });
-
+	queuedColors.set(`${gridX},${gridY}`, color);
 	cooldown();
+	await createTile({ x: gridX, y: gridY, color: color, team });
+	await paintFromDatabase();
+	queuedColors.delete(`${gridX},${gridY}`);
 }
 
 function clampPos() {
-	let padding = 1.5 * (MAX_CELL_SIZE - cellSize);
+	let padding = 0; // 1.5 * (MAX_CELL_SIZE - cellSize);
 	posX = Math.max(
 		window.innerWidth - CANVAS_SIZE * cellSize - padding,
 		Math.min(padding, posX)
@@ -116,23 +123,20 @@ function clampPos() {
 	);
 }
 
-window.addEventListener("resize", draw);
+window.addEventListener("resize", () => {
+	context.canvas.width = window.innerWidth;
+	context.canvas.height = window.innerHeight;
+	clampPos();
+	draw();
+});
 window.addEventListener("load", () => {
 	posX = window.innerWidth / 2 - (CANVAS_SIZE * cellSize) / 2;
 	posY = window.innerHeight / 2 - (CANVAS_SIZE * cellSize) / 2;
+	context.canvas.width = window.innerWidth;
+	context.canvas.height = window.innerHeight;
+	clampPos();
 	paintFromDatabase();
 	setInterval(paintFromDatabase, 1000);
-});
-
-window.addEventListener("mousemove", (e) => {
-	hoverX = Math.floor((e.clientX - posX) / cellSize);
-	hoverY = Math.floor((e.clientY - posY) / cellSize);
-	if (dragging) {
-		posX += e.movementX;
-		posY += e.movementY;
-		clampPos();
-	}
-	draw();
 });
 
 window.addEventListener("mousedown", (e) => {
@@ -142,11 +146,19 @@ window.addEventListener("mousedown", (e) => {
 		paint(e.clientX, e.clientY);
 	}
 });
-
+window.addEventListener("mousemove", (e) => {
+	hoverX = Math.floor((e.clientX - posX) / cellSize);
+	hoverY = Math.floor((e.clientY - posY) / cellSize);
+	if (dragging) {
+		posX += e.movementX;
+		posY += e.movementY;
+		clampPos();
+	}
+	window.requestAnimationFrame(draw);
+});
 window.addEventListener("mouseup", (e) => {
 	if (e.button !== 0) dragging = false;
 });
-
 window.addEventListener("wheel", (e) => {
 	let prevCellSize = cellSize;
 	cellSize -= Math.sign(e.deltaY) * ZOOM_SPEED;
@@ -159,5 +171,47 @@ window.addEventListener("wheel", (e) => {
 	posY -= gridY * (cellSize - prevCellSize);
 
 	clampPos();
-	draw();
+	window.requestAnimationFrame(draw);
+});
+
+window.addEventListener("touchstart", (e) => {
+	lastTouchPosX = e.touches[0].clientX;
+	lastTouchPosY = e.touches[0].clientY;
+	if (e.touches.length == 2) {
+		zooming = true;
+		lastTouchesDist = Math.hypot(
+			e.touches[0].clientX - e.touches[1].clientX,
+			e.touches[0].clientY - e.touches[1].clientY
+		);
+	}
+});
+window.addEventListener("touchmove", (e) => {
+	posX += e.touches[0].clientX - lastTouchPosX;
+	posY += e.touches[0].clientY - lastTouchPosY;
+	lastTouchPosX = e.touches[0].clientX;
+	lastTouchPosY = e.touches[0].clientY;
+	if (zooming) {
+		let touchesDist = Math.hypot(
+			e.touches[0].clientX - e.touches[1].clientX,
+			e.touches[0].clientY - e.touches[1].clientY
+		);
+		let prevCellSize = cellSize;
+		cellSize += Math.sign(touchesDist - lastTouchesDist) * ZOOM_SPEED;
+		cellSize = Math.max(cellSize, MIN_CELL_SIZE);
+		cellSize = Math.min(cellSize, MAX_CELL_SIZE);
+
+		let centerX = (e.touches[0].clientX + e.touches[1].clientX) / 2;
+		let centerY = (e.touches[0].clientY + e.touches[1].clientY) / 2;
+		let gridX = Math.floor((centerX - posX) / prevCellSize) + 1;
+		let gridY = Math.floor((centerY - posY) / prevCellSize) + 1;
+		posX -= gridX * (cellSize - prevCellSize);
+		posY -= gridY * (cellSize - prevCellSize);
+
+		lastTouchesDist = touchesDist;
+	}
+	clampPos();
+	window.requestAnimationFrame(draw);
+});
+window.addEventListener("touchend", (e) => {
+	if (e.touches.length == 2) zooming = false;
 });
